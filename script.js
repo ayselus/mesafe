5f17244845d89b5ac44a200cf0461846aa702d8c
ayselus <ayselabdullayeva804@gmail.com>
2024-07-01 18:16:13 +0400

    sjjs

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let mekan2 = prompt ('Getmek isdediyiniz mekani daxil edin;')
 let mesafe = prompt ('Yolun mesafesini qeyd edin;')
 let suret = prompt ('Suretinizi qeyd edin;')
 let saat = mesafe/suret
-let netice = mekan + ' ' + mekan2 + ' mesafe ' + mesafe + 'km/saat,suret ise ' + suret + "km/suret ile hereket edersek biz " + saat + 'saata catariq'
+let netice = mekan + ' dan ' + mekan2 + ' ye mesafe ' + mesafe + 'km ,suret ise ' + suret + "km/saat ile hereket edersek biz, " + saat + 'saata catariq'
 document.getElementById('netice').innerHTML = netice
